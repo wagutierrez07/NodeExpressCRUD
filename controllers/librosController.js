506dcd7108a1d68032a833c37d8a1d06eff08085
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-10 15:21:12 -0400

    CRUD Functions I

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -3,11 +3,15 @@ var libro = require("../models/Libro");
 
 module.exports = {
     index:function (req, res) {
-
         libro.obtener(conn, function(err,results){
             console.log(results);
+            res.render('libros/index', { title: 'Libro', libro:results });
         });
-
-        res.render('libros/index', { title: 'Libro' });
+        },
+        create:function (req,res) {
+            res.render('libros/crear');
+        },
+        update:function (req,res) {
+            res.render('libros/editar');
     }
 };
