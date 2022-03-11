1775c271a974866cb0dae6eced4759aefed2d903
Walter Gutierrez <elterrible8596@hotmail.com>
2022-03-11 13:18:37 -0400

    Adding bootstrap

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -5,11 +5,11 @@ module.exports = {
     index:function (req, res) {
         libro.obtener(conn, function(err,results){
             console.log(results);
-            res.render('libros/index', { title: 'Libro', libro:results });
+            res.render('libros/index', { title: 'Libros', libro:results });
         });
         },
         create:function (req,res) {
-            res.render('libros/crear');
+            res.render('libros/crear',  { title: 'Registrar Libros' });
         },
         update:function (req,res) {
             res.render('libros/editar');
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send("Bienvenido");
-})
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
+  
 
 module.exports = router;
